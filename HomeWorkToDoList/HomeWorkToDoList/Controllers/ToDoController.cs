4efb1f95aeb9e0b803a28e2f6a0f0e5d2044e704
PavelRnD <98314322+PavelRnD@users.noreply.github.com>
2024-10-11 13:11:58 +0300

    added edpoints

--- HomeWorkToDoList/HomeWorkToDoList/Controllers/ToDoController.cs
+++ HomeWorkToDoList/HomeWorkToDoList/Controllers/ToDoController.cs
@@ -1,32 +1,50 @@
+using HomeWorkToDoList.DataAccessLayer;
 using HomeWorkToDoList.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HomeWorkToDoList.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class ToDoController : ControllerBase
-    {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
+    { 
         private readonly ILogger<ToDoController> _logger;
+        private readonly ToDoListContext _context;
 
-        public ToDoController(ILogger<ToDoController> logger)
+        public ToDoController(ILogger<ToDoController> logger, ToDoListContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [HttpGet]
-        public IEnumerable<ToDo> Get()
+        public async Task<IEnumerable<ToDo>> List()
         {
-            return Enumerable.Range(1, 5).Select(index => new ToDo
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)) 
-            })
-            .ToArray();
+            return await _context.ToDoList
+                .OrderBy(todo => todo.Date)
+                .ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<Guid> Create([FromBody] ToDo toDo)
+        {
+            if (toDo.Id == Guid.Empty)
+                toDo.Id = Guid.NewGuid();
+
+            await _context.ToDoList.AddAsync(toDo);
+            await _context.SaveChangesAsync();
+            return toDo.Id;
+        }
+
+        [HttpPut]
+        public async Task Update([FromBody] ToDo toDo)
+        {
+            var foundedToDo = await _context.ToDoList.FirstOrDefaultAsync(t => t.Id == toDo.Id) 
+                ?? throw new Exception("todo not found");
+            foundedToDo.Date = toDo.Date;
+            foundedToDo.Note = toDo.Note; 
+            await _context.SaveChangesAsync(); 
         }
     }
 }

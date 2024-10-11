db18d4f42706f085535f0e7d971fcb3ed4918aa4
PavelRnD <98314322+PavelRnD@users.noreply.github.com>
2024-10-11 12:52:05 +0300

    fix model

--- HomeWorkToDoList/HomeWorkToDoList/Controllers/ToDoController.cs
+++ HomeWorkToDoList/HomeWorkToDoList/Controllers/ToDoController.cs
@@ -24,9 +24,7 @@ namespace HomeWorkToDoList.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new ToDo
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)) 
             })
             .ToArray();
         }
--- HomeWorkToDoList/HomeWorkToDoList/Models/ToDo.cs
+++ HomeWorkToDoList/HomeWorkToDoList/Models/ToDo.cs
@@ -4,12 +4,8 @@ namespace HomeWorkToDoList.Models
     {
         public Guid Id { get; set; }
 
-        public DateOnly Date { get; set; }
+        public DateOnly Date { get; set; } 
 
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
+        public string? Note { get; set; }
     }
 }
